4ccf615c68012817cdd760dce809e1611eef0d2f
Laura Verdoes <l.verdoes@decos.com>
2019-02-28 14:54:31 +0100

    Add docker compose file for stack
    Note that Visual Studio docker tools use docker-compose which does not support 'deploy' and that `docker stack` does not support 'build'.

--- TestSendCore/Program.cs
+++ TestSendCore/Program.cs
@@ -8,33 +8,45 @@ namespace TestSendCore
     {
         static void Main(string[] args)
         {
-            System.Diagnostics.Trace.AutoFlush = true;
-            System.Diagnostics.Trace.Listeners.Add(new LogstashHttpTraceListener("http://log-dev.decos.nl:9090"));
-
-            var logFactory = new TraceSourceLogFactory();
-            var log = logFactory.Create<Program>();
-
-            log.Debug("Debug message.");
-            log.Debug(new { data = "Debug data", data2 = 1 });
-
-            log.Info("Info message.");
-            log.Info(new { data = "Info data", data2 = 2 });
-
-            log.Warn("Warning message.");
-            log.Warn(new { data = "Warning data", data2 = 3 });
+            Console.WriteLine("Starting.");
 
             try
             {
-                log.Error("Error message.");
-                throw new Exception();
-            }
-            catch(Exception ex)
-            {
-                log.Error(new { exception = ex, message = "Error message." });
-            }
+                System.Diagnostics.Trace.AutoFlush = true;
+                System.Diagnostics.Trace.Listeners.Add(new LogstashHttpTraceListener("http://log-dev.decos.nl:9090"));
 
-            log.Critical("Critical message.");
-            log.Critical(new { data = "Critical data", data2 = 4 });
+                var logFactory = new TraceSourceLogFactory();
+                var log = logFactory.Create<Program>();
+
+                log.Debug("Debug message.");
+                log.Debug(new { data = "Debug data", data2 = 1 });
+
+                log.Info("Info message.");
+                log.Info(new { data = "Info data", data2 = 2 });
+
+                log.Warn("Warning message.");
+                log.Warn(new { data = "Warning data", data2 = 3 });
+
+                try
+                {
+                    log.Error("Error message.");
+                    throw new Exception();
+                }
+                catch (Exception ex)
+                {
+                    log.Error(new { exception = ex, message = "Error message." });
+                }
+
+                log.Critical("Critical message.");
+                log.Critical(new { data = "Critical data", data2 = 4 });
+
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unhandled exception!");
+                Console.WriteLine(ex);
+            }
         }
     }
 }
