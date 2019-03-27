91c58351a087e06d7fb4137ca5676d0a0d5287ff
Laura Verdoes <l.verdoes@decos.com>
2019-03-27 14:20:13 +0100

    Add support for logging both a message and structured data at the same time.

---
+++ Decos.Diagnostics/LogData{T}.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Decos.Diagnostics
+{
+    /// <summary>
+    /// Represents a logged message with additional structured data.
+    /// </summary>
+    public class LogData<T> : LogData
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogData{T}"/> class with
+        /// the specified message and data.
+        /// </summary>
+        /// <param name="message">The text of the logged message.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public LogData(string message, T data)
+            : base(message, data)
+        {
+        }
+
+        /// <summary>
+        /// Gets an object that provides additional data.
+        /// </summary>
+        public new T Data => (T)base.Data;
+    }
+}
--- Decos.Diagnostics/LogExtensions.cs
+++ Decos.Diagnostics/LogExtensions.cs
@@ -8,12 +8,31 @@ namespace Decos.Diagnostics
     public static class LogExtensions
     {
         /// <summary>
+        /// Writes a message to the log with the specified severity and additional data.
+        /// </summary>
+        /// <param name="log">The log to write to.</param>
+        /// <param name="logLevel">The severity of the message.</param>
+        /// <param name="message">The text of the message to log.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public static void Write<T>(this ILog log, LogLevel logLevel, string message, T data)
+            => log.Write(logLevel, new LogData<T>(message, data));
+
+        /// <summary>
         /// Logs a verbose message for development or debugging purposes. These are not enabled in production by default and should only be enabled temporarily for troubleshooting.
         /// </summary>
         /// <param name="log">The log to write to.</param>
         /// <param name="message">The text of the message to log.</param>
         public static void Debug(this ILog log, string message)
             => log.Write(LogLevel.Debug, message);
+
+        /// <summary>
+        /// Logs a verbose message for development or debugging purposes. These are not enabled in production by default and should only be enabled temporarily for troubleshooting.
+        /// </summary>
+        /// <param name="log">The log to write to.</param>
+        /// <param name="message">The text of the message to log.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public static void Debug<T>(this ILog log, string message, T data)
+            => log.Write(LogLevel.Debug, message, data);
 
         /// <summary>
         /// Logs verbose data for development or debugging purposes. These are not enabled in production by default and should only be enabled temporarily for troubleshooting.
@@ -25,12 +44,21 @@ namespace Decos.Diagnostics
             => log.Write(LogLevel.Debug, data);
 
         /// <summary>
-        /// Logs an informational message. These  are typically used to track the general flow of an application and should have long-term value and.
+        /// Logs an informational message. These are typically used to track the general flow of an application and should have long-term value and.
         /// </summary>
         /// <param name="log">The log to write to.</param>
         /// <param name="message">The text of the message to log.</param>
         public static void Info(this ILog log, string message)
             => log.Write(LogLevel.Information, message);
+
+        /// <summary>
+        /// Logs an informational message. These are typically used to track the general flow of an application and should have long-term value and.
+        /// </summary>
+        /// <param name="log">The log to write to.</param>
+        /// <param name="message">The text of the message to log.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public static void Info<T>(this ILog log, string message, T data)
+            => log.Write(LogLevel.Information, message, data);
 
         /// <summary>
         /// Logs informational data. These are typically used to track the general flow of an application and should have long-term value .
@@ -50,13 +78,13 @@ namespace Decos.Diagnostics
             => log.Write(LogLevel.Warning, message);
 
         /// <summary>
-        /// Logs a recoverable error that does not cause the execution of an operation to stop but might need to be investigated later.
+        /// Logs a warning message highlighting an abnormal or unexpected event, such as a recoverable error that does not cause the execution of an operation to stop but might need to be investigated later.
         /// </summary>
         /// <param name="log">The log to write to.</param>
-        /// <param name="context">A message that provides context for the error.</param>
-        /// <param name="exception">The exception to log.</param>
-        public static void Warn(this ILog log, string context, Exception exception)
-            => log.Warn(new ExceptionData(context, exception));
+        /// <param name="message">The text of the message to log.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public static void Warn<T>(this ILog log, string message, T data)
+            => log.Write(LogLevel.Warning, message, data);
 
         /// <summary>
         /// Logs data highlighting an abnormal or unexpected event.
@@ -76,13 +104,13 @@ namespace Decos.Diagnostics
             => log.Write(LogLevel.Error, message);
 
         /// <summary>
-        /// Logs an error that causes the execution of an operation to stop.
+        /// Logs a message indicating a failure that causes the execution of an operation to stop.
         /// </summary>
         /// <param name="log">The log to write to.</param>
-        /// <param name="context">A message that provides context for the error.</param>
-        /// <param name="exception">The exception to log.</param>
-        public static void Error(this ILog log, string context, Exception exception)
-            => log.Error(new ExceptionData(context, exception));
+        /// <param name="message">The text of the message to log.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public static void Error<T>(this ILog log, string message, T data)
+            => log.Write(LogLevel.Error, message, data);
 
         /// <summary>
         /// Logs data indicating a failure that causes the execution of an operation to stop.
@@ -102,13 +130,13 @@ namespace Decos.Diagnostics
             => log.Write(LogLevel.Critical, message);
 
         /// <summary>
-        /// Logs an error that requires immediate attention.
+        /// Logs a message that requires immediate attention.
         /// </summary>
         /// <param name="log">The log to write to.</param>
-        /// <param name="context">A message that provides context for the error.</param>
-        /// <param name="exception">The exception to log.</param>
-        public static void Critical(this ILog log, string context, Exception exception)
-            => log.Critical(new ExceptionData(context, exception));
+        /// <param name="message">The text of the message to log.</param>
+        /// <param name="data">An object that provides additional data.</param>
+        public static void Critical<T>(this ILog log, string message, T data)
+            => log.Write(LogLevel.Critical, message, data);
 
         /// <summary>
         /// Logs data that requires immediate attention.
